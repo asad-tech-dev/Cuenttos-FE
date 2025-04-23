5451d62eb69d158532b2975f78b315d2a18d6323
asad-tech-dev <sidtechdevpk@gmail.com>
2025-04-23 16:28:14 +0500

    changed hero section
    Signed-off-by: asad-tech-dev <sidtechdevpk@gmail.com>

--- src/app/components/landing/Hero.tsx
+++ src/app/components/landing/Hero.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import VioletButton from "../buttons/VioletButton";
-import TextButton from "../buttons/textButton";
+import { AnimatedGradientTextDemo } from "./animatedtext";
 import OnboardHeader from "../headers/onboardHeader";
 
 export default function Hero() {
@@ -18,30 +18,33 @@ export default function Hero() {
       <div className="absolute inset-0 bg-gradient-to-l from-darkpurple-grad to-purple-grad z-0" />
       <div className="relative z-10 h-full flex flex-col">
         <OnboardHeader />
-        <main className="flex items-center h-screen md:justify-end justify-center w-full -mt-[55px] md:pl-[980px] pr-[20px] md:pl-[0px] pl-[20px]">
+        <main className="flex items-center h-screen justify-center w-full -mt-[55px] pr-[20px] md:pl-[0px] pl-[20px]">
           <div className="flex flex-1 items-center justify-center px-4 ">
-            <div className="flex flex-col gap-[30px] w-[409px] h-[296px]">
-              <h1 className="text-[45px] text-white font-normal leading-[52px]">
+            <div className="flex flex-col justify-center items-center gap-[10px]">
+              <div className="flex items-center justify-center">
+                <AnimatedGradientTextDemo />
+              </div>
+              <h1 className="text-[65px] text-white font-semibold text-center leading-[72px] w-[600px]">
                 Wellness through Writing.
               </h1>
-              <p className="text-[16px] text-offwhite font-normal leading-[24px]">
+              <p className="text-[16px] text-center text-offwhite font-normal mt-[20px] leading-[24px] w-[500px]">
                 In a world dominated by dopamine generating algorithms, Cuentto
                 provides a safe haven for connecting with yourself and others
                 through writing.
               </p>
-              <div className="flex flex-row gap-4 items-center justify-start">
+              <div className="flex flex-row gap-4 items-center justify-start mt-[20px]">
                 <Link href="/login">
                   <VioletButton
-                    text="Write a Cuentto"
+                    text="Get Started"
                     className="w-[155px] text-[14px]"
                   />
                 </Link>
-                <Link href="/login">
+                {/* <Link href="/login">
                   <TextButton
                     text="Explore Cuentto"
                     className="w-[133px] h-[40px]"
                   />
-                </Link>
+                </Link> */}
               </div>
             </div>
           </div>
